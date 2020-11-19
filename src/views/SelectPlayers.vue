09596f2eb70a5f19bef595a83e02e6cae90028c7
Gitesh <writemail2gitesh@gmail.com>
2020-11-19 12:53:39 +0530

    hooked up firebase with vuex for common module.

--- src/views/SelectPlayers.vue
+++ src/views/SelectPlayers.vue
@@ -1,13 +1,12 @@
 <template>
-  <div class="w-full h-full bg-fff6eb grid grid-rows-3 place-items-center bg-selection-bg bg-center bg-cover">
+  <div class="w-full h-full bg-fff6eb grid grid-rows-3 place-items-center">
     <img src="../assets/logo-principle.png" />
-    <div class="text-white -mt-20 -ml-20">
-      <p class="text-2xl font-lg font-kalam">Welcome to</p>
-      <p class="text-2xl">Siem Reap Mini
-        <br />Golf!</p>
+    <div>
+      <p>Welcome to</p>
+      <p>Siem Reap Mini Golf!</p>
     </div>
-    <div class="space-y-4 flex flex-col justify-between">
-      <p class="text-xl uppercase text-005D63"> How many players ?</p>
+    <div class="space-y-4">
+      <p>How many players?</p>
       <InputRange
         :min="1"
         :max="6"
@@ -15,11 +14,14 @@
         @changed="selectedPlayers"
       />
       {{ noOfPlayers }}
-
-    <base-button
-    mode="confirm"
-    to="NamePlayers"
-    >Confirm</base-button>
+    </div>
+    <div class="w-full text-center">
+      <button
+        class="cursor-pointer focus:outline-none"
+        @click="selectNumOfPlayers"
+      >
+        Confirm
+      </button>
     </div>
   </div>
 </template>
@@ -51,6 +53,6 @@ export default {
     selectNumOfPlayers() {
       commonRefs.set({ numOfPlayers: this.noOfPlayers });
     }
-  },
+  }
 };
 </script>
