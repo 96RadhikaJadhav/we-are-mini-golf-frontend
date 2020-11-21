95ab327913c85630451ebeb5fc93b441564ad23a
Gitesh <writemail2gitesh@gmail.com>
2020-11-21 17:25:51 +0530

    hotfix merge code

--- src/components/utilities/BaseCard.vue
+++ src/components/utilities/BaseCard.vue
@@ -1,9 +1,5 @@
 <template>
-<<<<<<< HEAD
   <div class="bg-white p-4 shadow-md rounded-3xl w-full">
-=======
-  <div class="bg-white p-2 shadow-md rounded-3xl">
->>>>>>> origin/staging
     <slot></slot>
   </div>
 </template>
