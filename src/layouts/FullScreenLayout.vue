bf5ab84e2295d714d6cb28e78080d40936549cfa
Danny Venn <freedannyv@gmail.com>
2020-12-02 15:05:04 +0700

    transition working by adding appear property

--- src/layouts/FullScreenLayout.vue
+++ src/layouts/FullScreenLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-full w-full">
-    <transition name="fade">
+    <transition name="fade" appear>
       <router-view class="w-full mx-auto box-border min-h-screen" />
     </transition>
   </div>
