078e2c17c0667d862cbd1f54dcf660fcbec9b0af
Gitesh <writemail2gitesh@gmail.com>
2021-02-15 10:30:45 +0530

    fixed infinite loop on routes

--- src/views/NamePlayers.vue
+++ src/views/NamePlayers.vue
@@ -73,7 +73,7 @@ export default {
     startGame() {
       this.updateGameDetails({ playersInfo: this.playersInfo })
         .then(() => {
-          this.$router.push({ name: 'SelectCourse' });
+          this.$router.push({ name: 'GameCourse' });
         })
         .catch(e => console.log(e));
     }
