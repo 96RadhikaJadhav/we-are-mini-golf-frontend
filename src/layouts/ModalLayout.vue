0f32835f133ff5a49c2290e3ea4bec51f77131fc
Danny Venn <freedannyv@gmail.com>
2021-02-18 17:43:41 +0700

    18 - help us modal updated

--- src/layouts/ModalLayout.vue
+++ src/layouts/ModalLayout.vue
@@ -5,22 +5,23 @@
     <ErrorMessage v-if="invalid" />
 
     <!-- Main Card -->
-    <div class="relative bg-fff6eb rounded-3xl shadow-md w-full max-w-sm px-4">
-      <!-- Closing Button -->
-      <div v-if="closingButton" class="flex justify-end pt-2">
-        <button
-          class="text-aeb49a text-2xl font-bold focus:outline-none cursor-pointer mb-1"
-          @click="$emit('close')"
-        >
-          &#10005;
-        </button>
-      </div>
+    <div class="w-full relative bg-fff6eb rounded-3xl shadow-md max-w-sm">
+      <div class="px-4">
+        <!-- Closing Button -->
+        <div v-if="closingButton" class="flex justify-end pt-2">
+          <button
+            class="text-aeb49a text-2xl font-bold focus:outline-none cursor-pointer mb-1"
+            @click="$emit('close')"
+          >
+            &#10005;
+          </button>
+        </div>
 
-      <!-- Content -->
-      <div>
-        <slot></slot>
+        <!-- Content -->
+        <div>
+          <slot></slot>
+        </div>
       </div>
-
       <div
         class="bg-ea9864 text-white font-kalam rounded-3xl text-center text-xl leading-tight"
       >
